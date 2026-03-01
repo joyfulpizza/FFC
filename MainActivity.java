e12a7f4c2ce04dde7d61cc721dd809d6408a55b3
joyfulpizza <donggyui417@gmail.com>
2026-03-01 10:22:46 +0900

    Add overlay permission handling in MainActivity
    Implement overlay permission check and start FPS service.

---
+++ MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.floatingfps;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                !Settings.canDrawOverlays(this)) {
+
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        } else {
+            startService(new Intent(this, FPSService.class));
+            finish();
+        }
+    }
+}
